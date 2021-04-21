42c83533424100b63d649dec4d74c61debb846f5
kimchijeon <phanjenny@outlook.com>
2021-04-21 15:10:22 +0100

    Example Form apge added to test unit testing of its Controller

--- view/header.php
+++ view/header.php
@@ -24,9 +24,9 @@ use function Mos\Functions\url;
         <a href="<?= url("/") ?>">Home</a> |
         <a href="<?= url("/session") ?>">Session</a> |
         <a href="<?= url("/debug") ?>">Debug</a> |
-        <a href="<?= url("/twig") ?>">Twig view</a> |
         <a href="<?= url("/some/where") ?>">some/where</a> |
         <a href="<?= url("/no/such/path") ?>">Show 404 example</a> |
+        <a href="<?= url("/form/view") ?>">Form view</a> |
         <a href="<?= url("/game21") ?>">Game 21</a> |
         <a href="<?= url("/yatzy") ?>">Yatzy</a>
     </nav>
